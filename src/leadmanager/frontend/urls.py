95abd98255bf4af7eb500107ccbd2fc1348ee499
Frantz <fpaul787@gmail.com>
2020-06-08 12:17:18 -0400

    last commit

--- src/leadmanager/frontend/urls.py
+++ src/leadmanager/frontend/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
+# urlpatterns = [
+#     path('', views.index)
+# ]
+
+# OR...
 urlpatterns = [
-    path('', views.index)
+    path('', TemplateView.as_view(template_name='frontend/index.html'))
 ]
